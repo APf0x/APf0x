27d24bb7d29ae8ec90c35d58b9eede8b89d7b251
APf0x <77631767+APf0x@users.noreply.github.com>
2023-10-31 22:28:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import markdown
+
+
+def fetch_daily_quote():
+    response = requests.get('https://zenquotes.io/api/today')
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data[0]['q']
+    else:
+        return None
+
+with open("README.MD", 'r') as file:
+    markdown_content = file.read()
+
+
+daily_quote = fetch_daily_quote()
+
+if daily_quote:
+
+    html_content = markdown.markdown(markdown_content)
+    
+
+    html_content = html_content.replace('<!-- Daily Quote Placeholder -->', f'<blockquote>{daily_quote}</blockquote>')
+    
+
+    modified_markdown = markdown.markdown(html_content)
+
+
+
+    
+
+    with open("README.MD", 'w') as file:
+        file.write(modified_markdown)
